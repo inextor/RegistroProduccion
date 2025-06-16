0d508a6fb06425257a8c5c771a4e4c3e4424fdb2
nextor <nextor@gmail.com>
2025-06-16 14:43:20 -0700

    Se agregaron los filtros por fecha y se agrego indicador de carga de la red

--- src/app/production-by-group/production-by-group.component.ts
+++ src/app/production-by-group/production-by-group.component.ts
@@ -4,6 +4,7 @@ import { Production } from '../RestClases/Production';
 import { combineLatest, mergeMap, Observable, of, startWith } from 'rxjs';
 import { ActivatedRoute, ParamMap, Router, RouterLink, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule} from '@angular/forms';
 
 
 interface LocationParameters
@@ -14,7 +15,7 @@ interface LocationParameters
 
 @Component({
 	selector: 'app-production-by-group',
-	imports: [RouterModule, CommonModule],
+	imports: [RouterModule, CommonModule,FormsModule],
 	templateUrl: './production-by-group.component.html',
 	styleUrl: './production-by-group.component.css'
 })
@@ -28,12 +29,29 @@ export class ProductionByGroupComponent
 	total_qty:number = 0;
 	total_alternate_qty:number = 0;
 	total_average:number = 0;
+	is_loading:boolean = false;
+
+	group_id:string = '';
 
 
 
 	constructor(private rest_service:RestService,private route:ActivatedRoute,private router:Router)
 	{
 		this.production = new Production(rest_service);
+	}
+
+	onStartDateChange(date_string:string)
+	{
+		this.start_date = date_string;
+		this.loadData(this.group_id, this.start_date, this.end_date);
+		console.log(this.start_date);
+	}
+
+	onEndDateChange(date_string:string)
+	{
+		this.end_date = date_string;
+		this.loadData(this.group_id, this.start_date, this.end_date);
+		console.log(this.end_date);
 	}
 
 	ngOnInit()
@@ -51,7 +69,9 @@ export class ProductionByGroupComponent
 				this.start_date = this.production.getLocalMysqlStringFromDate(start).substring(0,10)
 				this.end_date = this.production.getLocalMysqlStringFromDate(end).substring(0,10)
 
-				return of( data.params.get('id') as string )
+				this.group_id = data.params.get('id') as string;
+
+				return of( this.group_id );
 			})
 		)
 		.subscribe
@@ -72,9 +92,9 @@ export class ProductionByGroupComponent
 	loadData(group_id:string, start_date:string, end_date:string)
 	{
 
+		this.is_loading = true;
 		let s = this.production.getDateFromLocalMysqlString(start_date);
 		s.setHours(0,0,0,0);
-
 
 		let e = this.production.getDateFromLocalMysqlString(end_date);
 		e.setHours(23,59,59,59);
@@ -95,6 +115,14 @@ export class ProductionByGroupComponent
 			this.total_qty = total_qty;
 			this.total_alternate_qty = total_alternate_qty;
 			this.total_average = total_qty/total_alternate_qty;
+		})
+		.catch((error:any) =>
+		{
+			this.rest_service.showError(error);
+		})
+		.finally(()=>
+		{
+			this.is_loading = false;
 		});
 	}
 
