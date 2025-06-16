5f627720bcc8717acaac1710bd6f3273c8bd53c4
nextor <nextor@gmail.com>
2025-06-16 11:32:23 -0700

    Se agrego deteccion de cambios en el url
    Se agregaron los diferentes url para la navegacion

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,75 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router, RouterLink } from '@angular/router';
+import { combineLatest, mergeMap, Observable, of, startWith } from 'rxjs';
+import { RestService } from '../rest.service';
+
+interface LocationParameters
+{
+	query:ParamMap,
+	params:ParamMap
+}
 
 @Component({
-  selector: 'app-header',
-  imports: [RouterLink],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+	selector: 'app-header',
+	imports: [RouterLink],
+	templateUrl: './header.component.html',
+	styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent
+{
+
+	is_logged_in: boolean = false;
+
+	constructor(private route:ActivatedRoute,private router:Router,private rest_service:RestService)
+	{
+
+	}
+
+	logout()
+	{
+		localStorage.clear();
+		this.rest_service.is_logged_in = false;
+		this.router.navigate(['/login']);
+	}
+
+	ngOnInit()
+	{
+		this.getQueryParamObservable()
+		.subscribe
+		(
+			(data:LocationParameters) =>
+			{
+				this.is_logged_in = localStorage.getItem('session') !== null;
+			}
+		);
+	}
+
+
+	getQueryParamObservable():Observable<LocationParameters>
+	{
+		let p:ParamMap = {
+			has:(_prop)=>false,
+			keys:[],
+			get:(_value:string)=>{ return null},
+			getAll:()=>{ return []},
+		};
+
+		return combineLatest
+		([
+			this.route.queryParamMap.pipe(startWith(p)),
+			this.route.paramMap
+		])
+		.pipe
+		(
+			mergeMap((foo:ParamMap[]) =>
+			{
+				return of
+				({
+					query: foo[0],
+					params: foo[1]
+				});
+			})
+		);
+	}
 
 }
