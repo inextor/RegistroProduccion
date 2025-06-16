6ae85e13a3aba428614a9d9ef17a8fd779d06cf0
nextor <nextor@gmail.com>
2025-06-16 11:38:09 -0700

    Diferente correcciones en funciones

--- src/app/RestClases/Production.ts
+++ src/app/RestClases/Production.ts
@@ -1,12 +1,17 @@
+import { Utils } from "../classes/DateUtils";
 import { RestService } from "../rest.service";
 
 
 export class Production
 {
-
 	constructor(private rest_service: RestService)
 	{
 
+	}
+
+	zero(x:number):string
+	{
+		return x < 10 ? '0'+x : ''+x;
 	}
 
 	async getProductionAreas(storeId: number): Promise<any>
@@ -42,7 +47,7 @@ export class Production
 			.then(data => data.data)
 			.then(items =>
 			{
-				let item_ids = items.map((item:any) => item.id);
+				let item_ids = items.map((item:any) => item.item_id);
 				let url_items = `${this.rest_service.getBaseUrl()}/item.php?id,=${item_ids.join(',')}&limit=999999`;
 
 				return fetch( url_items , options )
@@ -162,12 +167,20 @@ export class Production
 
 	getProductionItemInfoByItemId(item_id: any,date:string):Promise<any[]>
 	{
-		let d = new Date();
+		let d = Utils.getDateFromLocalMysqlString(date);
+		d.setHours(0,0,0,0);
 
-		//convert to UTC
+		let end = new Date();
+		end.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+		end.setSeconds(end.getSeconds() - 1);
+
+
+		let start_utc_string = d.toISOString().substring(0,19).replace('T',' ');
+		let end_utc_string = end.toISOString().substring(0,19).replace('T',' ');
 
 		let options = { method: 'GET', headers: { 'Authorization': `Bearer ${this.rest_service.session.id}` } };
-		const url = `${this.rest_service.getBaseUrl()}/production_info.php?item_id=${item_id}&limit=999999`;
+		const url = `${this.rest_service.getBaseUrl()}/production_info.php?item_id=${item_id}&created>=${start_utc_string}&created<=${end_utc_string}&limit=999999&_sort_order=id_DESC`;
+
 		return fetch(url, options )
 			.then(this.getJsonLambda())
 			.then(data => data.data)
@@ -177,6 +190,67 @@ export class Production
 	{
 		let options = { method: 'GET', headers: { 'Authorization': `Bearer ${this.rest_service.session.id}` } };
 		const url = `${this.rest_service.getBaseUrl()}/item_info.php?id=${item_id}`;
+		return fetch(url, options )
+			.then(this.getJsonLambda())
+	}
+
+	updateProduction(production:any):Promise<any>
+	{
+		let headers =	{
+			'Authorization': `Bearer ${this.rest_service.session.id}`,
+			'Content-Type': 'application/json'
+		};
+
+		let method = 'PUT';
+		let body = JSON.stringify(production);
+		let options = { method, body, headers };
+		const url = `${this.rest_service.getBaseUrl()}/production.php`;
+
+		return fetch(url, options )
+			.then(this.getJsonLambda())
+			.then(data => data.data)
+	}
+
+
+	getLocalMysqlStringFromDate(date:Date):string
+	{
+		let d= new Date();
+		d.setTime(date.getTime());
+
+		let event_string = d.getFullYear()
+		+ '-' + this.zero(d.getMonth() + 1)
+		+ '-' + this.zero(d.getDate())
+		+ ' ' + this.zero(d.getHours())
+		+ ':' + this.zero(d.getMinutes())
+		+ ':' + this.zero(d.getSeconds());
+
+		return event_string;
+	}
+
+	getDateFromLocalMysqlString(str:string):Date
+	{
+		let components = str.split(/-|:|\s|T/g);
+
+		let f:number[] = [];
+
+		f.push( parseInt(components[0]) );
+		f.push( parseInt(components[1])-1 );
+		f.push( parseInt(components[2]) );
+		f.push( components.length<4?0:parseInt(components[3]))
+		f.push( components.length<5?0:parseInt(components[4]))
+		f.push( components.length<6?0:parseInt(components[5]))
+
+		return new Date( f[0], f[1], f[2], f[3], f[4], f[5], 0);
+	}
+
+	getProductionByGroup(group_id:string, start_date:Date, end_date:Date):Promise<any[]>
+	{
+
+		let start_utc_string = start_date.toISOString().substring(0,19).replace('T',' ');
+		let end_utc_string = end_date.toISOString().substring(0,19).replace('T',' ');
+
+		let options = { method: 'GET', headers: { 'Authorization': `Bearer ${this.rest_service.session.id}` } };
+		const url = `${this.rest_service.getBaseUrl()}/reports/production_by_area.php?production_area_id=${group_id}&start=${start_utc_string}&end=${end_utc_string}`;
 		return fetch(url, options )
 			.then(this.getJsonLambda())
 	}
