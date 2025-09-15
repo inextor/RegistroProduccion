cae82c8ecc38616bd015adbacff3e5c93f3ea96a
nextor <nextor@gmail.com>
2025-09-15 16:07:52 -0700

    Agregando nomina alterno

--- src/app/generar-nomina-alterno/generar-nomina-alterno.component.ts
+++ src/app/generar-nomina-alterno/generar-nomina-alterno.component.ts
@@ -6,21 +6,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RestConsumption } from '../RestClases/RestConsumption';
 import { ShortDatePipe } from '../pipes/short-date.pipe';
+import { Payroll_Value } from '../Models/Payroll_Value';
+import { Payroll } from '../Models/Payroll';
+import { User } from '../Models/User';
+import { ProductionAreaInfo } from '../Models/ProductionAreaInfo';
+import { mergeMap } from 'rxjs';
 
-interface NominaConcept
+interface PayrollInfo
 {
-	description:string;
-	date: string;
-	qty:number;
-	price:number;
-	total:number;
+	values: Payroll_Value[];
+	user: User;
+	payroll:Payroll;
 }
 
-interface UserConcepts {
-  user: any;
-  concepts: NominaConcept[];
-  total: number;
-}
 
 interface ProductionDetail {
   date: string;
@@ -55,11 +53,14 @@ export class GenerarNominaAlternoComponent implements OnInit {
 	end_date: string = '';
 	totalesPorProducto: any[] = [];
 	user_list: any[] = [];
-	user_concepts_list: UserConcepts[] = [];
+	payroll_info_list: PayrollInfo[] = [];
 	production_detail_list: ProductionDetail[] = [];
 	item_total_list: ItemTotal[] = [];
 	total_pieces: number = 0;
 	total_kg: number = 0;
+
+	production_area_info:ProductionAreaInfo | null = null;
+	user_extra_deductions:Map<number,Payroll_Value[]> = new Map();
 
 	constructor(public rest_service: RestService, public route: ActivatedRoute, public router:Router) {
 		this.rest_production = new RestProduction(rest_service);
@@ -72,7 +73,8 @@ export class GenerarNominaAlternoComponent implements OnInit {
 			this.end_date = params.get('end_date') || '';
 			let index = 'production_area_id';
 			this.production_area_id = params.has(index) ? parseInt( params.get(index) ):'';
-			this.searchProductionAreaData();
+			if(!(this.production_area_id == '' || this.start_date == '' || this.end_date =='' ))
+				this.searchProductionAreaData();
 		});
 
 		this.rest_production.getAllProductionAreas()
@@ -80,9 +82,6 @@ export class GenerarNominaAlternoComponent implements OnInit {
 				this.production_area_list = data;
 			});
 
-		if (this.production_area_id && this.start_date && this.end_date) {
-				this.searchProductionAreaData();
-		}
 	}
 
 	submitSearch(evt:any)
@@ -153,72 +152,95 @@ export class GenerarNominaAlternoComponent implements OnInit {
 	}
 
 	agruparYCalcularTotales() {
-		this.user_concepts_list = [];
+		this.payroll_info_list = [];
 
 		for (const user of this.user_list) {
-			const user_concepts: UserConcepts = {
-				user: user,
-				concepts: [],
+			const payroll: Payroll = {
+				id: 0,
+				store_id: 1,
+				start_date: this.start_date,
+				end_date: this.end_date,
+				status: 'ACTIVE',
+				created: new Date().toISOString().slice(0, 19).replace('T', ' '),
+				paid_status: 'PENDING',
+				subtotal: 0,
 				total: 0
 			};
 
-			const concepts_map = new Map<string, NominaConcept>();
+			const payroll_values_map = new Map<string, Payroll_Value>();
 
+			// Producción
 			for (const pi of this.production_info_list) {
 				for (const pu of pi.users) {
 					if (pu.user_id === user.id) {
 						const date = pi.production.produced.substring(0, 10);
 						const key = `Producción-${date}`;
 
-						let concept = concepts_map.get(key);
+						let payroll_value = payroll_values_map.get(key);
 
-						if (!concept) {
-							concept = {
-								description: 'Producción',
-								date: date,
-								qty: 0,
-								price: 0, // Price is not relevant for the grouped concept
-								total: 0
+						if (payroll_value == undefined) {
+
+							payroll_value = {
+								id: 0,
+								payroll_id: 0,
+								description: 'Producción del día ' + date,
+								type: 'PERCEPTION',
+								value: 0,
+								datetime: null,
+								status: 'ACTIVE',
+								created: date
 							};
-							concepts_map.set(key, concept);
+
+							payroll_values_map.set(key, payroll_value as Payroll_Value);
 						}
 
-						concept.total += pi.production.qty * pu.price;
+						payroll_value.value += pi.production.qty * pu.price;
 					}
 				}
 			}
 
+			// Consumos
 			for (const ci of this.consumption_info_list) {
 				for (const cu of ci.users) {
 					if (cu.user_id === user.id && cu.price && cu.price > 0) {
 						const date = ci.consumption.consumed.substring(0, 10);
 						const key = `${ci.item.name}-${date}`;
 
-						let concept = concepts_map.get(key);
+						let payroll_value:Payroll_Value | undefined = payroll_values_map.get(key);
 
-						if (!concept) {
-							concept = {
-								description: ci.item.name,
-								date: date,
-								qty: 0,
-								price: cu.price,
-								total: 0
+						if (payroll_value == undefined) {
+							payroll_value = {
+								id: 0,
+								payroll_id: 0,
+								description: 'CONSUMO',
+								type: 'DEDUCTION',
+								datetime: null,
+								value: 0,
+								status: 'ACTIVE',
+								created: date
 							};
-							concepts_map.set(key, concept);
+							payroll_values_map.set(key, payroll_value as Payroll_Value);
 						}
-
-						console.log('QTY is '+cu.total, ci.consumption.consumed );
-
-						concept.qty += ci.consumption.qty;
-						concept.total -= cu.total;
+						payroll_value.value+= cu.total;
 					}
 				}
 			}
 
-			user_concepts.concepts = Array.from(concepts_map.values());
-			user_concepts.total = user_concepts.concepts.reduce((acc, c) => acc + c.total, 0);
+			const payroll_values = Array.from(payroll_values_map.values());
+			payroll_values.sort((a, b) => a.description.localeCompare(b.description));
 
-			this.user_concepts_list.push(user_concepts);
+			const subtotal = payroll_values.filter(pv => pv.type === 'PERCEPTION').reduce((acc, pv) => acc + pv.value, 0);
+			const deductions = payroll_values.filter(pv => pv.type === 'DEDUCTION').reduce((acc, pv) => acc + pv.value, 0);
+			payroll.subtotal = subtotal;
+			payroll.total = subtotal - deductions;
+
+			const payroll_info: PayrollInfo = {
+				user: user,
+				payroll: payroll,
+				values: payroll_values
+			};
+
+			this.payroll_info_list.push(payroll_info);
 		}
 
 		const production_detail_map = new Map<string, ProductionDetail>();
@@ -269,5 +291,38 @@ export class GenerarNominaAlternoComponent implements OnInit {
 
 		this.total_pieces = this.item_total_list.reduce((acc, item) => acc + item.pieces, 0);
 		this.total_kg = this.item_total_list.reduce((acc, item) => acc + item.kg, 0);
+	}
+
+
+	savePayroll()
+	{
+		let payroll_info_array = [];
+
+		this.rest_production.getProductionAreaInfo( this.production_area_id  as number)
+		.then((production_area_info:ProductionAreaInfo)=>
+		{
+			let payroll_info_array = production_area_info.users.map((production_user)=>{
+				let payroll:Payroll= {
+					id: 0,
+					created:'',
+					end_date:this.end_date,
+					paid_status: 'PENDING',
+					start_date:this.start_date,
+					status: 'ACTIVE',
+					store_id: 1,
+					subtotal: 0,
+					total: 0,
+				}
+
+				let payroll_values:Payroll_Value[] = [];
+
+				let payroll_info:Partial<PayrollInfo> = {
+					payroll,
+					values: payroll_values
+				}
+
+				return payroll_info;
+			});
+		});
 	}
 }
