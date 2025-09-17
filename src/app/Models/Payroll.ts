048554d300fad85b7bdf28c25a63290ea365ff16
nextor <nextor@gmail.com>
2025-09-17 02:21:08 -0700

    Adding user_id to the model payroll

--- src/app/Models/Payroll.ts
+++ src/app/Models/Payroll.ts
@@ -6,6 +6,7 @@ export interface Payroll
 	paid_status: 'PENDING'|'PAID',
 	start_date:string;
 	status: 'ACTIVE'|'DELETED';
+	user_id:number;
 	store_id: number;
 	subtotal: number;
 	total: number;
