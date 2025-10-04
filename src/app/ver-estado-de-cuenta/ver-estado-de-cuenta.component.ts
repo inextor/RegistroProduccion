1b95aa2a2706b3a383ea4ebfeba35bc00e8ded59
nextor <nextor@gmail.com>
2025-10-03 17:12:32 -0700

    refactor(estado-de-cuenta): Improve data fetching and component structure
    - Refactors the getData method to fetch user and account data in parallel using Promise.all, improving performance.
    
    - Updates the component to handle cases where an account might not exist for a user.
    
    - Adds a user property to the component and the CData interface to store user information.
    
    - Introduces a getEmptyUser method to provide a default user object.
    
    - Updates the getEmptyAccount method to return an account with id: -1 to better handle non-existent accounts.
    
    - Adds CommonModule and RouterModule to the component's imports.

--- src/app/ver-estado-de-cuenta/ver-estado-de-cuenta.component.ts
+++ src/app/ver-estado-de-cuenta/ver-estado-de-cuenta.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RestService } from '../rest.service';
 import { Rest, RestResponse } from '../classes/Rest';
 import { Account } from '../Models/Account';
 import { Utils } from '../classes/DateUtils';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Ledger } from '../Models/Ledger';
+import { User } from '../Models/User';
 
 interface CData
 {
+	user: User;
 	account: Account;
-	ledgers: any[];
+	ledgers: Ledger[];
 }
 
 @Component({
 	selector: 'app-ver-estado-de-cuenta',
-	imports: [],
+	standalone: true,
+	imports: [CommonModule, RouterModule],
 	templateUrl: './ver-estado-de-cuenta.component.html',
 	styleUrl: './ver-estado-de-cuenta.component.css'
 })
@@ -24,67 +29,74 @@ export class VerEstadoDeCuentaComponent implements OnInit {
 	rest_account: Rest;
 	account: Account=this.getEmptyAccount();
 	ledgers: any[] = [];
+	user: User=this.getEmptyUser();
 
 	constructor(private rest_service:RestService,private route:ActivatedRoute) {
-		this.rest_user = new Rest(rest_service,'/user.php');
-		this.rest_ledger = new Rest(rest_service,'/ledger.php');
-		this.rest_account = new Rest(rest_service,'/account.php');
+		this.rest_user = new Rest(rest_service,'/user');
+		this.rest_ledger = new Rest(rest_service,'/ledger');
+		this.rest_account = new Rest(rest_service,'/account');
 	}
 
 	ngOnInit(): void {
 		this.route.queryParamMap.subscribe((params) =>
 		{
-			let page = params.has('page') ? parseInt(params.get('page') as string) : 1;
+			let page = params.has('page') ? parseInt(params.get('page') as string) : 0;
 			let limit = params.has('limit') ? parseInt(params.get('limit') as string) : 100;
 			let user_id = params.has('user_id') ? parseInt(params.get('user_id') as string) : 0;
 
-			this.getData(user_id, page, limit)
-			.then((response)=>{
-				this.account =  response.account;
-				this.ledgers = response.ledgers;
-			})
-			.catch((error) =>
-			{
-				this.rest_service.showError(error);
-			})
+            this.getData(user_id, page, limit)
+            .then((response)=>{
+				this.user = response.user;
+                this.account =  response.account;
+                this.ledgers = response.ledgers;
+            })
+            .catch((error) =>
+            {
+                this.rest_service.showError(error);
+            });
 		});
 	}
 
 
 	getData(user_id:number, page:number, limit:number):Promise<CData>
 	{
-		return this.rest_account.search({user_id:user_id, currency_id:'MXN',limit: 1})
-		.then((response:RestResponse<Account>)=>
+		return Promise.all
+		([
+			this.rest_user.get(user_id),
+			this.rest_account.search({user_id:user_id, currency_id:'MXN', limit:999999})
+		])
+		.then(([user,response]:[User,RestResponse<Account>])=>
 		{
-			let account_ids = response.data.map(a => a.id);
+			response.data.sort((_a,b)=>b.is_main ? 1 : -1);
 
-			let p_ledgers = account_ids.length > 0
-				? this.rest_ledger.search({account_id:account_ids, page, limit})
-				: Promise.resolve({total:0,data:[]});
-
-			return Promise.all([ response.data, p_ledgers ]);
-		})
-		.then(([accounts,ledgers]) =>
-		{
-			if( accounts.length === 0 )
+			if( response.data.length )
 			{
-				return Promise.resolve({
-					account: this.getEmptyAccount(),
-					ledgers: []
-				});
+				return Promise.all([user, response.data[0]]);
 			}
 
-			let account = accounts[0];
+			return Promise.resolve([ user, this.getEmptyAccount() ]) as Promise<[User,Account]>;
+		})
+		.then(([user,account]:[User,Account])=>
+		{
+			let p_ledgers = account.id > 0
+				? this.rest_ledger.search({account_id:account.id, page, limit, _sort:'created_ASC'}) as Promise<RestResponse<Ledger>>
+				: Promise.resolve({total:0,data:[]} as RestResponse<Ledger>);
 
-			return { account, ledgers: ledgers.data };
+			return Promise.all([user,account, p_ledgers]) as Promise<[User, Account, RestResponse<Ledger>]>;
+		})
+		.then(([user, account, ledgers]: [User, Account, RestResponse<Ledger>]) =>
+		{
+			ledgers.data.sort((a,b)=>a.created.localeCompare(b.created));
+			return Promise.resolve({user, account:account, ledgers: ledgers.data });
 		})
 	}
 
 	getEmptyAccount():Account
 	{
 		return {
-			id: 0,
+			id: -1,
 			user_id: 0,
+			is_main: false,
 			currency_id: 'MXN',
 			balance: 0,
 			created: Date.toString().substring(0,19).replace('T',' '),
@@ -93,4 +105,34 @@ export class VerEstadoDeCuentaComponent implements OnInit {
 			updated_by_user_id: 0,
 		};
 	}
+
+	getEmptyUser(): User {
+		return {
+            id: 0,
+            username: '',
+            password: '',
+            created: Date.toString().substring(0, 19).replace('T', ' '),
+            updated: Date.toString().substring(0, 19).replace('T', ' '),
+            created_by_user_id: 0,
+            updated_by_user_id: 0,
+            credit_days: null,
+            credit_limit: 0,
+            default_billing_address_id: null,
+            default_shipping_address_id: null,
+            email: null,
+            image_id: null,
+            lat: null,
+            lng: null,
+            name: '',
+            phone: null,
+            platform_client_id: null,
+            points: 0,
+            price_type_id: null,
+            production_area_id: null,
+            status: 'ACTIVE',
+            store_id: null,
+            type: 'CLIENT',
+            workshift_id: null
+        };
+    }
 }
