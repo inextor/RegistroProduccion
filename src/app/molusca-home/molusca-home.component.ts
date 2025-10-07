d93b7556819345170ea40e1b874d7ac14122281b
nextor <nextor@gmail.com>
2025-10-07 11:07:42 -0700

    Agregando una pagina defualt por si no tienen permisos los usuarios tengadn donde llegar y no genere error: Implementacion de permisos

---
+++ src/app/molusca-home/molusca-home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-molusca-home',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './molusca-home.component.html',
+  styleUrl: './molusca-home.component.css'
+})
+export class MoluscaHomeComponent {
+
+}
